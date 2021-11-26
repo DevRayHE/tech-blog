a40507f56c43570f787c6eee7af48cc0ea723964
Ray He <devrayhe@gmail.com>
2021-11-26 23:40:45 +1100

    route /blog/:id
    Implemented route /blog/:id tested working fine, with link on views  blog.handlebars functioning as expected

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,4 +25,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/blog/:id', async (req, res) => {
+  try {
+    // Get blog data with match id from req body
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plan: true });
+
+    res.render('blog', {
+      ...blog
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
